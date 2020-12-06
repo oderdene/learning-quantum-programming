bfe30658ee8259fddf23ca94b568ff413d6a8dbf
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-12-06 23:45:53 +0800

    generated full adder truth table using quantum algorithm

---
+++ add_two_numbers.py
@@ -0,0 +1,81 @@
+import sys
+import cirq
+
+print("Бүхэл тоо оруулна уу!")
+
+a = int(input("a="))
+b = int(input("b="))
+
+n_bits = 3
+a_bits = '{:08b}'.format(a)[-n_bits:]
+b_bits = '{:08b}'.format(b)[-n_bits:]
+
+print("{}={} {}={}".format(a, a_bits, b, b_bits))
+
+
+def sum_qubits(a_bit, b_bit, carry_in):
+    q0, q1, q2, q3, q4, q5, q6, q7 = cirq.LineQubit.range(8)
+    # q0 qubit-ийг а битд зориулж хэрэглэнэ
+    # q1 qubit-ийг б битд зориулж хэрэглэнэ
+    # q2 qubit-ийг carry bit-д зориулна
+    def assign_bit(bit, qubit):
+        if bit==1:
+            yield cirq.X(qubit)
+
+    circuit = cirq.Circuit(
+            assign_bit(a_bit, q0),
+            assign_bit(b_bit, q1),
+            assign_bit(carry_in, q2),
+            # AND1
+            cirq.TOFFOLI(q0, q1, q3),
+            # XOR1
+            cirq.CNOT(q0, q4),
+            cirq.CNOT(q1, q4),
+            # XOR2
+            cirq.CNOT(q2, q5),
+            cirq.CNOT(q4, q5),
+            # AND2
+            cirq.TOFFOLI(q2, q4, q6),
+            # OR
+            cirq.X(q3),
+            cirq.X(q6),
+            cirq.TOFFOLI(q3, q6, q7),
+            cirq.X(q7), # carry out
+            cirq.measure(q5, q7, key='results')
+            )
+    simulator = cirq.Simulator()
+    result    = simulator.run(circuit, repetitions=100).measurements['results'][0]
+    return result[0], result[1]
+
+
+a_bit, b_bit, carry_in = 1, 1, 1
+summed, carry_out = sum_qubits(a_bit=a_bit, b_bit=b_bit, carry_in=carry_in)
+print("a={} b={} carry_in={} sum={} carry_out={}".format(a_bit, b_bit, carry_in, summed, carry_out))
+
+a_bit, b_bit, carry_in = 0, 1, 1
+summed, carry_out = sum_qubits(a_bit=a_bit, b_bit=b_bit, carry_in=carry_in)
+print("a={} b={} carry_in={} sum={} carry_out={}".format(a_bit, b_bit, carry_in, summed, carry_out))
+
+a_bit, b_bit, carry_in = 1, 0, 1
+summed, carry_out = sum_qubits(a_bit=a_bit, b_bit=b_bit, carry_in=carry_in)
+print("a={} b={} carry_in={} sum={} carry_out={}".format(a_bit, b_bit, carry_in, summed, carry_out))
+
+a_bit, b_bit, carry_in = 0, 0, 1
+summed, carry_out = sum_qubits(a_bit=a_bit, b_bit=b_bit, carry_in=carry_in)
+print("a={} b={} carry_in={} sum={} carry_out={}".format(a_bit, b_bit, carry_in, summed, carry_out))
+
+a_bit, b_bit, carry_in = 1, 1, 0
+summed, carry_out = sum_qubits(a_bit=a_bit, b_bit=b_bit, carry_in=carry_in)
+print("a={} b={} carry_in={} sum={} carry_out={}".format(a_bit, b_bit, carry_in, summed, carry_out))
+
+a_bit, b_bit, carry_in = 0, 1, 0
+summed, carry_out = sum_qubits(a_bit=a_bit, b_bit=b_bit, carry_in=carry_in)
+print("a={} b={} carry_in={} sum={} carry_out={}".format(a_bit, b_bit, carry_in, summed, carry_out))
+
+a_bit, b_bit, carry_in = 1, 0, 0
+summed, carry_out = sum_qubits(a_bit=a_bit, b_bit=b_bit, carry_in=carry_in)
+print("a={} b={} carry_in={} sum={} carry_out={}".format(a_bit, b_bit, carry_in, summed, carry_out))
+
+a_bit, b_bit, carry_in = 0, 0, 0
+summed, carry_out = sum_qubits(a_bit=a_bit, b_bit=b_bit, carry_in=carry_in)
+print("a={} b={} carry_in={} sum={} carry_out={}".format(a_bit, b_bit, carry_in, summed, carry_out))
