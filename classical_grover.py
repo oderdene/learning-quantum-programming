922e2b005bf731360dc5456bd23e50577ddb136e
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-12-07 23:49:02 +0800

    simplest grover algorithm

--- classical_grover.py
+++ classical_grover.py
@@ -6,13 +6,30 @@ def oracle(x, search_element):
         return 1
     return 0
 
+def solve(search_element):
+    for element in elements:
+        if oracle(element, search_element):
+            return element
+    return None
 
 if __name__ == "__main__":
     print("Элементүүд")
     print(elements)
+
     search_element = 3
     print("Хайх утга", search_element)
-    for element in elements:
-        print(element, oracle(element, search_element))
+    result = solve(search_element=search_element)
+    if result:
+        print(result, "элемент олдлоо")
+    else:
+        print("олдсонгүй")
+
+    search_element = 40
+    print("Хайх утга", search_element)
+    result = solve(search_element=search_element)
+    if result:
+        print(result, "элемент олдлоо")
+    else:
+        print("олдсонгүй")
 
 
