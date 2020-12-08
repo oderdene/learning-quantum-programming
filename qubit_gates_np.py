a84c149f2915e081cdb11c5a4fc85bf149e95285
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-12-09 02:26:20 +0800

    representing multiple qubit states using Kronecker Product aka Tensor Product

--- qubit_gates_np.py
+++ qubit_gates_np.py
@@ -72,3 +72,24 @@ print(hadamard_q0, "\n")
 hadamard_q1 = np.dot(hadamard, q1)
 print("hadamard q1")
 print(hadamard_q1, "\n")
+
+
+# |001> = |0>⊗ |0>⊗ |1>
+print("##### Олон qubit-үүдийн төлөв \n")
+
+# |00>
+q00 = np.kron(zero, zero)
+print("|00>")
+print(q00, "\n")
+# |01>
+q01 = np.kron(zero, one)
+print("|01>")
+print(q01, "\n")
+# |10>
+q10 = np.kron(one, zero)
+print("|10>")
+print(q10, "\n")
+# |11>
+q11 = np.kron(one, one)
+print("|11>")
+print(q11, "\n")
