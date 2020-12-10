2c5075d562c121ad57ffde9f4280030fb7ba16f2
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-12-10 08:49:56 +0800

    added permutation matrixes of basic quantum gates

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Just learning quantum stuffs
  - Үндсэн квант gate-үүдийг numpy дээр хэрэгжүүлэлж харах [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/drive/1lTTULxuOql2VDmjPZ5Qcdeitpy3KFfRW?usp=sharing)
 
 # Tasks
- - ONGOIN : Implement AND gate or Toffoli gate with numpy
+ - ONGOING : Implement AND gate or Toffoli gate with numpy
  - Implement OR gate with numpy
  - Implement quantum full adder with numpy
  - Implement addition of two integers with numpy
--- and_gate_with_toffoli_np.py
+++ and_gate_with_toffoli_np.py
@@ -1,4 +1,50 @@
 import numpy as np
-import sympy as sp
 
+
+def normalize_state(state):
+    return state/np.linalg.norm(state)
+
+def n_kron(*args):
+    result = np.array([[1.+0.j]])
+    for op in args:
+        result = np.kron(result, op)
+    return result
+
+# |0>
+zero = np.array([[1.+0.j],
+                 [0.+0.j]], dtype=np.cfloat)
+# |1>
+one  = np.array([[0.+0.j],
+                 [1.+0.j]], dtype=np.cfloat)
+# minus |-> = 1/√2(|0>-|1>)
+minus = normalize_state(zero-one)
+# plus  |+> = 1/√2(|0>+|1>)
+plus  = normalize_state(zero+one)
+
+# Quantum logic gates
+# - https://en.wikipedia.org/wiki/Quantum_logic_gate
+pauli_x = np.array(
+        [[0.+0.j, 1.+0.j],
+         [1.+0.j, 0.+0.j]])
+pauli_y = np.array(
+        [[0.+0.j, -1j    ],
+         [1j    ,  0.+0.j]])
+pauli_z = np.array(
+        [[1.+0.j,  0.+0.j],
+         [0.+0.j, -1.+0.j]])
+hadamard = np.array(
+        [[1.+0.j,  1.+0.j],
+         [1.+0.j, -1.+0.j]]
+        )*1/np.sqrt(2.+0.j)
+ID2 = np.eye(2, dtype=np.cfloat)
+toffoli = np.array(
+        [[1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 1, 0, 0, 0, 0, 0],
+         [0, 0, 0, 1, 0, 0, 0, 0],
+         [0, 0, 0, 0, 1, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 1],
+         [0, 0, 0, 0, 0, 0, 1, 0]],
+        dtype=np.cfloat)
 
