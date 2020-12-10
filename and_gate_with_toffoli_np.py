73a66f22b423b3806a325c23f23b30f9ad9454df
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-12-10 09:03:29 +0800

    about to implement AND gate some input testing

--- and_gate_with_toffoli_np.py
+++ and_gate_with_toffoli_np.py
@@ -16,9 +16,9 @@ zero = np.array([[1.+0.j],
 # |1>
 one  = np.array([[0.+0.j],
                  [1.+0.j]], dtype=np.cfloat)
-# minus |-> = 1/√2(|0>-|1>)
+# |-> = 1/√2(|0>-|1>)
 minus = normalize_state(zero-one)
-# plus  |+> = 1/√2(|0>+|1>)
+# |+> = 1/√2(|0>+|1>)
 plus  = normalize_state(zero+one)
 
 # Quantum logic gates
@@ -48,3 +48,25 @@ toffoli = np.array(
          [0, 0, 0, 0, 0, 0, 1, 0]],
         dtype=np.cfloat)
 
+
+def AND(first_bit, second_bit):
+    q0 = zero
+    q1 = zero
+    if first_bit==1:
+        q0 = np.dot(pauli_x, q0)
+    if second_bit==1:
+        q1 = np.dot(pauli_x, q1)
+    print(q0)
+    print(q1)
+    return None
+
+
+if __name__=="__main__":
+    print("|00>")
+    res = AND(0, 0)
+    print("|01>")
+    res = AND(0, 1)
+    print("|10>")
+    res = AND(1, 0)
+    print("|11>")
+    res = AND(1, 1)
