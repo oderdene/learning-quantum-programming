9dda0d1e42fb31efe75ef6d3cbf06d5daadb23fc
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-12-10 13:17:06 +0800

    preparing to implement quantum full adder using numpy

---
+++ full_adder_np.py
@@ -0,0 +1,75 @@
+import random
+import numpy as np
+import sympy as sp
+from sympy.physics.quantum.qubit import matrix_to_qubit
+
+
+def normalize_state(state):
+    return state/np.linalg.norm(state)
+
+def n_kron(*args):
+    result = np.array([[1.+0.j]])
+    for op in args:
+        result = np.kron(result, op)
+    return result
+
+# |0>
+zero = np.array([[1.+0.j],
+                 [0.+0.j]], dtype=np.cfloat)
+# |1>
+one  = np.array([[0.+0.j],
+                 [1.+0.j]], dtype=np.cfloat)
+# |-> = 1/√2(|0>-|1>)
+minus = normalize_state(zero-one)
+# |+> = 1/√2(|0>+|1>)
+plus  = normalize_state(zero+one)
+
+# Quantum logic gates
+# - https://en.wikipedia.org/wiki/Quantum_logic_gate
+pauli_x = np.array(
+        [[0.+0.j, 1.+0.j],
+         [1.+0.j, 0.+0.j]])
+pauli_y = np.array(
+        [[0.+0.j, -1j    ],
+         [1j    ,  0.+0.j]])
+pauli_z = np.array(
+        [[1.+0.j,  0.+0.j],
+         [0.+0.j, -1.+0.j]])
+hadamard = np.array(
+        [[1.+0.j,  1.+0.j],
+         [1.+0.j, -1.+0.j]]
+        )*1/np.sqrt(2.+0.j)
+ID2 = np.eye(2, dtype=np.cfloat)
+toffoli = np.array(
+        [[1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 1, 0, 0, 0, 0, 0],
+         [0, 0, 0, 1, 0, 0, 0, 0],
+         [0, 0, 0, 0, 1, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 1],
+         [0, 0, 0, 0, 0, 0, 1, 0]],
+        dtype=np.cfloat)
+
+
+def measure(amplitudes, repetitions=10):
+    measurements = []
+
+    for _ in range(repetitions):
+        weights   = [abs(amplitude)**2 for amplitude in amplitudes]
+        outcome   = random.choices(range(len(amplitudes)), weights)[0]
+        new_state = np.zeros((len(amplitudes), 1))
+        new_state[outcome][0] = 1
+        measurements.append(new_state)
+
+    sample = random.choice(measurements)
+    qubit  = list(matrix_to_qubit(np.array(sample)).free_symbols)[0]
+
+    return qubit.qubit_values
+
+
+
+
+
+if __name__=="__main__":
+    pass
