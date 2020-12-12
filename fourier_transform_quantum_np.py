21afd19530622a4c287655191cf7c9933857dc69
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-12-12 20:53:34 +0800

    fourier transform numpy

--- fourier_transform_quantum_np.py
+++ fourier_transform_quantum_np.py
@@ -67,13 +67,6 @@ swap = np.array(
          [0, 0, 0, 1]],
         dtype=np.cfloat)
 
-
-# Qubit measurement
-#
-#   Qubit-ийн amplitude-ийн дагуу классик бит гаргаж авах
-#   тухайн битрүү collapse хийгдэж ирэх магадлал amplitude-аар илэрхийлэгдэнэ
-#
-#
 def measure(amplitudes, repetitions=10):
     measurements = []
     for _ in range(repetitions):
@@ -86,34 +79,25 @@ def measure(amplitudes, repetitions=10):
     qubit  = list(matrix_to_qubit(np.array(sample)).free_symbols)[0]
     return qubit.qubit_values
 
-# N qubit системд хэрэглэх NOT
-#
-#   apply_pauli_x(|10001>, 2, n=5) => |10101>
-#   apply_pauli_x(|10101>, 4, n=5) => |10100>
-#
-#
-def apply_pauli_x(psi, loc, n=8):
+def apply_pauli_x(psi, loc, n):
     op_list      = [ID2]*n
     op_list[loc] = pauli_x
     op_matrix    = n_kron_list(op_list)
     return np.dot(op_matrix, psi)
 
-# Хоёр qubit систем дээр
-#
-#   P0 = |0><0|, P1 = |1><1|
-#   CNOT = P0⊗ ID + P1⊗ X
-#
-# Олон qubit систем дээр жишээ нь 10 qubit систем дээр 3-р qubit нь
-# control qubit, 9-р qubit нь target qubit бол CNOT матриц дараах
-# байдлаар үүсгэгдэнэ.
-#
-#   Хэрэв 3-р qubit |0> бол 9-р qubit-ийг хэвээр үлдээнэ
-#     I⊗I⊗|0⟩⟨0|⊗I⊗I⊗I⊗I⊗I⊗I⊗I
-#   эсрэгээрээ |1> бол
-#     I⊗I⊗|1⟩⟨1|⊗I⊗I⊗I⊗I⊗I⊗X⊗I
-#
-#
-def apply_cnot(psi, control_loc, target_loc, n=8):
+def apply_pauli_z(psi, loc, n):
+    op_list      = [ID2]*n
+    op_list[loc] = pauli_z
+    op_matrix    = n_kron_list(op_list)
+    return np.dot(op_matrix, psi)
+
+def apply_hadamard(psi, loc, n):
+    op_list      = [ID2]*n
+    op_list[loc] = hadamard
+    op_matrix    = n_kron_list(op_list)
+    return np.dot(op_matrix, psi)
+
+def apply_cnot(psi, control_loc, target_loc, n):
     P0                     = np.dot(zero, zero.T)
     P1                     = np.dot(one , one.T )
     op_list_0              = [ID2]*n
@@ -124,24 +108,85 @@ def apply_cnot(psi, control_loc, target_loc, n=8):
     op_matrix = n_kron_list(op_list_0)+n_kron_list(op_list_1)
     return np.dot(op_matrix, psi)
 
-# N qubit системд хэрэглэх SWAP
-#
-#   apply_swap(|10110>, 1, 3, n=5) => |11100>
-#   apply_swap(|11010>, 0, 4, n=5) => |01011>
-#
-#
-def apply_swap(psi, a_loc, b_loc, n=8):
+def apply_swap(psi, a_loc, b_loc, n):
     psi = apply_cnot(psi, a_loc, b_loc, n=n)
     psi = apply_cnot(psi, b_loc, a_loc, n=n)
     psi = apply_cnot(psi, a_loc, b_loc, n=n)
     return psi
 
+def apply_cz(psi, control_loc, target_loc, n):
+    P0                     = np.dot(zero, zero.T)
+    P1                     = np.dot(one , one.T )
+    op_list_0              = [ID2]*n
+    op_list_0[control_loc] = P0
+    op_list_1              = [ID2]*n
+    op_list_1[control_loc] = P1
+    op_list_1[ target_loc] = pauli_z
+    op_matrix = n_kron_list(op_list_0)+n_kron_list(op_list_1)
+    return np.dot(op_matrix, psi)
+
+def apply_cz_rot(psi, control_loc, target_loc, rotation, n):
+    P0                     = np.dot(zero, zero.T)
+    P1                     = np.dot(one , one.T )
+    op_list_0              = [ID2]*n
+    op_list_0[control_loc] = P0
+    op_list_1              = [ID2]*n
+    op_list_1[control_loc] = P1
+    op_list_1[ target_loc] = pauli_z**rotation
+    op_matrix = n_kron_list(op_list_0)+n_kron_list(op_list_1)
+    return np.dot(op_matrix, psi)
+
+def apply_cz_and_swap(psi, a_loc, b_loc, rotation, n):
+    psi = apply_cz_rot(psi, a_loc, b_loc, rotation, n=n)
+    psi = apply_swap(psi, a_loc, b_loc, n=n)
+    return psi
+
 
 # Quantum fourier transform
+#
+#
+#  ─H───@^0.5───×───H────────────@^0.5─────×───H──────────@^0.5──×─H
+#       │       │                │         │               │     │
+#  ─────@───────×───@^0.25───×───@─────────×───@^0.25───×──@─────×──
+#                   │        │                 │        │
+#  ─────────────────┼────────┼───@^0.125───×───┼────────┼───────────
+#                   │        │   │         │   │        │
+#  ─────────────────@────────×───@─────────×───@────────×───────────
+#
+#
+# Сүүлийн төлөв
+# [0.25+0.j 0.25+0.j 0.25+0.j 0.25+0.j 0.25+0.j 0.25+0.j 0.25+0.j 0.25+0.j
+#  0.25+0.j 0.25+0.j 0.25+0.j 0.25+0.j 0.25+0.j 0.25+0.j 0.25+0.j 0.25+0.j]
+#
+#
 def fourier_transform():
     psi = n_kron_list([zero]*4)
-    print("\n\nQuantum fourier transform\n")
-    print(psi)
+
+    print("\nQuantum fourier transform\n")
+    print("\n", matrix_to_qubit(psi), "\n")
+
+    psi = apply_hadamard(psi, 0, n=4)
+
+    psi = apply_cz_and_swap(psi, 0, 1,   0.5, n=4)
+    psi = apply_cz_and_swap(psi, 1, 2,  0.25, n=4)
+    psi = apply_cz_and_swap(psi, 2, 3, 0.125, n=4)
+
+    psi = apply_hadamard(psi, 0, n=4)
+
+    psi = apply_cz_and_swap(psi, 0, 1,  0.5, n=4)
+    psi = apply_cz_and_swap(psi, 1, 2, 0.25, n=4)
+
+    psi = apply_hadamard(psi, 0, n=4)
+
+    psi = apply_cz_and_swap(psi, 0, 1,  0.5, n=4)
+
+    psi = apply_hadamard(psi, 0, n=4)
+
+    print("\nqFT(|0000>)\n")
+    print(matrix_to_qubit(psi))
+    qubit_values = measure([a[0] for a in psi])
+    print(qubit_values)
+
     pass
 
 
